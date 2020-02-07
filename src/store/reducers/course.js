96ee7f641dfa8b064e8820d3f9b6893023217dd2
RafaeloDuarte <rafaeloduarte@gmail.com>
2020-02-06 22:19:30 -0300

    A small POC to try to react-redux.

---
+++ src/store/reducers/course.js
@@ -0,0 +1,29 @@
+const INITIAL_STATE =
+{
+    activeLesson: {},
+    activeModule: {},
+    modules: [
+        {
+            id: 1, title: 'Iniciando com React', lessons: [
+                { id: 1, title: 'Aula 1' },
+                { id: 2, title: 'Aula 2' },
+            ]
+        },
+        {
+            id: 2, title: 'Iniciando com Redux', lessons: [
+                { id: 3, title: 'Aula 3' },
+                { id: 4, title: 'Aula 4' },
+            ]
+        }]
+}
+
+export default function course(state = INITIAL_STATE, action) {
+    if(action.type === 'TOGGLE_LESSON'){
+        return {
+            ...state, 
+            activeLesson: action.lesson,
+            activeModule: action.module
+        }
+    }
+    return state
+}
